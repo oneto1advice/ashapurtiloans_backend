61b775c8f5cd411e565e923baffdc4c7907be57a
99softtech <iopmobile@gllfl.com>
2024-09-18 19:00:00 +0530

    delete some

--- src/users-service/services/userService.ts
+++ src/users-service/services/userService.ts
@@ -28,8 +28,6 @@ export class UserService {
     async loginByEmailAndPassword(email: string, password: string): Promise<any | null> {
         const user = await this.userRepository.findByEmail(email);
         if (user === null) return { statusCode: 401, message: "User not Found", status: "failed" };
-        console.log(user)
-        console.log(user.status)
         if(user.status == 0) return { statusCode: 401, message: "Your account is disable. Please talk to Ashapurti Loans team", status: "failed" };
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return { statusCode: 401, message: "Password not match", status: "failed" };
@@ -69,9 +67,6 @@ export class UserService {
         const token = generateToken({ id: user.id, email: user.email });
         user.jwtToken = token;
         //user.resetPasswordExpires = new Date(Date.now() + 3600000); // 1 hour expiry
-      
-
-        // Send email
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
